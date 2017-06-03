ae1547c2a5a77dfdfc8921d3e39203f74b0251bf
Crystal <hello@crystalbohacek.com>
2017-06-03 00:37:45 -0400

    used geocoding API to separate state & city

--- routes/thriftstores.js
+++ routes/thriftstores.js
@@ -4,6 +4,9 @@ var Thriftstore = require("../models/thriftstore");
 var middleware = require("../middleware");
 var moment = require('moment');
 var config = require('../config.js');
+var request = require('request');
+var util = require('util');
+
 
 // console.log(config);
 
@@ -22,33 +25,67 @@ router.get("/", middleware.saveReferal, function(req, res){
 //create route - add thriftstore to database
 router.post("/", middleware.isLoggedIn, function(req, res){
    //get data from form and add to thriftstore array
-   
-   var newThriftstore = {
-       name: req.body.name,
-       image: req.body.image,
-       description: req.body.description,
-       address: req.body.address,
-       city: req.body.city,
-       state: req.body.state,
-       phone: req.body.phone,
-       pricegroup: req.body.pricegroup,
-       author: {
-           id: req.user._id,
-           username: req.user.username,
-           name: req.user.name,
-           email: req.user.email
-       }
-   };
-   //redirect back to thriftfinder page
-   //create a new thrift store and save to database
-   Thriftstore.create(newThriftstore, function(err, newlyCreated){
-       if(err){
-           console.log(err);
-       } else {
-           console.log(newlyCreated);
-           res.redirect("/thriftstores");
-       }
-   });
+   request('https://maps.googleapis.com/maps/api/geocode/json?key='
+           + config.api_keys.google_maps
+           + '&address=' + encodeURIComponent(req.body.address), function (error, response, body) {
+    if (error){
+      console.log('error!', error);
+    } else {
+    var data = JSON.parse(body);
+    console.log('data: ', util.inspect(data, {showHidden: false, depth: null}))
+
+    if (data.results && data.results[0] && ["address_components"]){
+        var addressComponents = data.results[0]["address_components"]
+        for (var i = 0; i < addressComponents.length; i++){
+            if(
+                addressComponents[i]['types'].indexOf('sublocality_level_1') > -1 ||
+                addressComponents[i]['types'].indexOf('locality') > -1){
+                var city = addressComponents[i]['long_name'];
+            }
+            if(addressComponents[i]['types'].indexOf('administrative_area_level_1') > -1){
+                var state = addressComponents[i]['short_name'];
+            }
+            if(addressComponents[i]['types'].indexOf('country') > -1){
+                var country = addressComponents[i]['long_name'];
+            }
+        }
+    }
+    else{
+        var city = null, state = null;
+    }
+
+
+
+     var newThriftstore = {
+         name: req.body.name,
+         image: req.body.image,
+         description: req.body.description,
+         address: req.body.address,
+         city: city,
+         state: state,
+         country: country,
+         phone: req.body.phone,
+         pricegroup: req.body.pricegroup,
+         author: {
+             id: req.user._id,
+             username: req.user.username,
+             name: req.user.name,
+             email: req.user.email
+         }
+     };
+     //redirect back to thriftfinder page
+     //create a new thrift store and save to database
+     Thriftstore.create(newThriftstore, function(err, newlyCreated){
+         if(err){
+             console.log(err);
+         } else {
+             console.log(newlyCreated);
+             res.redirect("/thriftstores");
+         }
+     });
+      }
+
+    });
    
 });
 
