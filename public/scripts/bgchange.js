f1f7c42ec684b37621ca31cdc188b6ab3fd9ca2e
Crystal Bohacek <hello@crystalbohacek.com>
2019-11-11 16:31:32 -0500

    fixed index background image

--- public/scripts/bgchange.js
+++
@@ -1,3 +0,0 @@
-//add more images for new background on reload
-var images = ['1.jpg'];
- $('#picture-header').css({'background': 'linear-gradient( rgba(20,20,20, .30), rgba(20,20,20, .30)), url(images/' + images[Math.floor(Math.random() * images.length)] + ')', 'background-size': 'cover', 'min-height': '75vh', 'margin-bottom': '30px'});
